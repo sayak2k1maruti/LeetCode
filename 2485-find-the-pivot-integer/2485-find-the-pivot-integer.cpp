f8d61e3d426853bacc8f7596e0deeb7659b6d526
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-03-14 00:55:51 +0530

    Time: 0 ms (100%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,17 +1,17 @@
+#include<cmath>
 class Solution {
 public:
     int pivotInteger(int n) {
-        int pivot = 1;
-        int left_sum = 0;
-        int right_sum = n*(n+1)/2;
-        while(pivot <= n){
-            left_sum+= pivot;
-            right_sum -= pivot;
-            if(left_sum == right_sum + pivot){
-                return pivot;
-            }
-            pivot++;
-        }
+        /*
+        p*(p+1)/2 = n*(n+1)/2 - (p-1)*p/2
+        p^2 + p = n^2 + n - p^2 + p
+        2p^2 = n^2 + n
+        p^2 = (n^2 + n)/2
+        p^2 = n*(n+1)/2
+        */
+        float pivot = sqrt(n*(n+1)/2);
+        if(pivot==(int)pivot)
+            return pivot;
         return -1;
     }
 };
