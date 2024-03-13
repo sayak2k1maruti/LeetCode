be1384797aa4c1a65a9a519bb0a84c7eb971bd1f
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-03-14 01:33:50 +0530

    Time: 18 ms (84.51%), Space: 29.1 MB (66.34%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,14 +1,14 @@
-#define MAX 10000
+#define MAX 10001
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int hash[MAX] = {0};
         for (int i = 0;i<nums.size(); i++)
-            hash[nums[i]*nums[i]] ++;
+            hash[abs(nums[i])] ++;
         int index=0;
         for(int i = 0; i<MAX;i++){
             for(int j=0;j<hash[i]; j++){
-                nums[index++] = i;
+                nums[index++] = i*i;
             }
         }
         return nums;
