92ffe6cd199878305d5a52556538261b57d78f17
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-03-14 01:32:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,30 +1,16 @@
+#define MAX 10000
 class Solution {
 public:
-    int partition(vector<int>&nums, int left,int right){
-        int pivot = nums[right];
-        int partition = left;
-        while(left < right){
-            if(nums[left] < pivot){
-                swap(nums[left],nums[partition]);
-                partition ++;
-            }
-            left++;
-        }
-        swap(nums[partition],nums[right]);
-        return partition;
-    }
-    void quick_sort(vector<int>&nums,int left, int right){
-        if (left<right){
-            int p = partition(nums,left,right);
-            quick_sort(nums,left,p-1);
-            quick_sort(nums,p+1,right);
-        }
-        return;
-    }
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0; i< nums.size(); i++)
-            nums[i] = nums[i] * nums[i];
-        quick_sort(nums,0,nums.size()-1);
+        int hash[MAX] = {0};
+        for (int i = 0;i<nums.size(); i++)
+            hash[nums[i]*nums[i]] ++;
+        int index=0;
+        for(int i = 0; i<MAX;i++){
+            for(int j=0;j<hash[i]; j++){
+                nums[index++] = i;
+            }
+        }
         return nums;
     }
 };
