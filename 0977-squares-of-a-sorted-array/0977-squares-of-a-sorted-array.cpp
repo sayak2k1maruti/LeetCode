e27c8ebac6b912a7d978694bc52cca29f784518e
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-03-14 01:55:09 +0530

    Time: 18 ms (84.51%), Space: 28.4 MB (84.35%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,16 +1,34 @@
 #define MAX 10001
 class Solution {
+
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-        int hash[MAX] = {0};
-        for (int i = 0;i<nums.size(); i++)
-            hash[abs(nums[i])] ++;
-        int index=0;
-        for(int i = 0; i<MAX;i++){
-            for(int j=0;j<hash[i]; j++){
-                nums[index++] = i*i;
+    vector<int> merge(vector<int>& nums, int partition){
+        vector<int> arr(nums.size());
+        int left_index = partition, right_index = partition+1,index=0;
+        while (left_index >= 0 && right_index < nums.size()){
+            if(nums[left_index] < nums[right_index]){
+                arr[index++] = nums[left_index]*nums[left_index];
+                left_index--;
+            }else{
+                arr[index++] = nums[right_index]*nums[right_index];
+                right_index++;             
             }
         }
-        return nums;
+        while(left_index >= 0){
+            arr[index++] = nums[left_index] * nums[left_index];
+            left_index--;
+        }
+        while(right_index < nums.size()){
+            arr[index++] = nums[right_index]*nums[right_index];
+            right_index++;
+        }
+        return arr;
+    }
+    vector<int> sortedSquares(vector<int>& nums) {
+        int partition = 0;
+        for(;partition<nums.size() && nums[partition] < 0;partition++){
+            nums[partition] = abs(nums[partition]);
+        }
+        return merge(nums,partition-1);
     }
 };
