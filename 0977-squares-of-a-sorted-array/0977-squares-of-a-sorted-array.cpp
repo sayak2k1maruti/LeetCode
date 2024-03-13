c3e7f27a7403f378e70441371d05838fbd191ad2
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-03-14 01:57:01 +0530

    Time: 15 ms (93.14%), Space: 28.4 MB (84.35%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> arr(nums.size());
         int left_index = partition, right_index = partition+1,index=0;
         while (left_index >= 0 && right_index < nums.size()){
-            if(-nums[left_index] < nums[right_index]){
+            if(abs(nums[left_index]) < nums[right_index]){
                 arr[index++] = nums[left_index]*nums[left_index];
                 left_index--;
             }else{
