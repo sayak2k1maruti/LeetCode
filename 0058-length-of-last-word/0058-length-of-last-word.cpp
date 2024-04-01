c2c8464786eda3d0c27ce1995c470c83af5f5a05
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-04-02 00:40:52 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.5%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        for(int i = s.length()-1 ; i >=0; i--){
+            if((s[i] == ' ') && (length != 0))
+                return length;
+            if (s[i] != ' ')
+                length++;
+        }
+        return length;
+    }
+};
