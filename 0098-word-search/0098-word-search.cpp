42f5381a0f2ae52dd5e365dbcf4ef31141ce28af
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-04-04 03:48:30 +0530

    Time: 32 ms (98.48%), Space: 9.8 MB (74.11%) - LeetHub

---
+++ 0098-word-search/0098-word-search.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool _exist(vector<vector<char>>& board, string& word,int i=0,int j=0, int index=0) {
+        if (i<0 || j <0 || i >= board.size() || j>=board[0].size())    return false;
+        if(index==word.length()-1)   return board[i][j] == word[index];
+        char temp = board[i][j];
+        if (word[index] == board[i][j]){
+            board[i][j] = '!';
+            if ( _exist(board,word,i+1,j,index+1)) return true;
+            if ( _exist(board,word,i,j+1,index+1))   return true;
+            if ( _exist(board,word,i-1,j,index+1))   return true;
+            if ( _exist(board,word,i,j-1,index+1))   return true;
+        } 
+        board[i][j] = temp;
+        return false ;
+    }
+    bool exist(vector<vector<char>>& board, string word){
+        if(word.length() > board.size() * board[0].size())  return false;
+        
+        //Checking if all charectors in the word are present in board [Optimization]
+        set<char> puzzle;
+        for(int i = 0; i < board.size();i++){
+            for(int j =0 ; j < board[0].size();j++){
+                puzzle.insert(board[i][j]);
+            }
+        }
+        for (int i = 0 ; i < word.length() ; i++){
+            if (puzzle.find(word[i]) == puzzle.end())   return false;
+        }
+        //
+
+        for(int i = 0; i < board.size();i++){
+            for(int j =0 ; j < board[0].size();j++){
+                if (board[i][j] == word[0] && _exist(board,word,i,j,0))  return true;
+            }
+        }
+        return false;
+    }
+};
