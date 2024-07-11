9917c056e13aa81a63d740492882103bba83d2ad
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-07-12 02:19:04 +0530

    Time: 3 ms (49.47%), Space: 11.6 MB (6.69%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        string innr = "";
         int left = 0;
         int right = 0;
         for (int i = 0 ; i < s.length(); i++){
-            if (s[i] == '(')    {left = i;innr = "";}
+            if (s[i] == '(')    {left = i;}
             else if(s[i]==')')  {right=i;break;}
-            else innr += s[i];
         }
         string left_s = left!=0 ? s.substr(0,left) : "";
         string right_s = right+1<s.length() ? s.substr(right+1,s.length()-right+1) : "";
-        if (innr.length()!=s.length()){
+        if (right){
+            string innr = s.substr(left+1,right-left-1);
             reverse(innr.begin(),innr.end());
             return reverseParentheses(left_s + innr + right_s);
         }
