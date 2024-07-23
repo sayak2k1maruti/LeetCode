b9e8e46e0d0e99bb8c4b008094a1b68177a82a61
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-07-24 00:01:20 +0530

    Time: 11 ms (17.92%), Space: 15.7 MB (5.99%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -3,18 +3,19 @@ public:
     int getIndex(int n) {return n + 100;}
     vector<int> frequencySort(vector<int>& nums) {
         map <int,int> frequency_map;
-        int max_occurs = 0;
+        //int max_occurs = 0;
         for (auto n : nums) {
             frequency_map[n] ++;
-            max_occurs = max(frequency_map[n],max_occurs);
+            //max_occurs = max(frequency_map[n],max_occurs);
         }
-        set <int> frequency_map_T[max_occurs+1];
+        set <int> frequency_map_T[200+1];
         //for(int i = 1; i <= max_occurs; i++)    frequency_map_T[i] = new set<int>;
+        
         for(auto it : frequency_map){
             frequency_map_T[it.second].insert(it.first);
         }
         int indx = 0;
-        for (int i = 1; i < max_occurs + 1; i++){
+        for (int i = 1; i < 200 + 1; i++){
             if (! frequency_map_T[i].empty()){
                 for(auto it = frequency_map_T[i].rbegin();it != frequency_map_T[i].rend();it++){
                     for(int j = 0; j < i ; j ++)    nums[indx++] = *it;
