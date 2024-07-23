3ae18a1fe91ef85347699d1a49c63b62b51934a6
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-07-24 00:01:55 +0530

    Time: 4 ms (73.1%), Space: 15.7 MB (5.99%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -3,14 +3,10 @@ public:
     int getIndex(int n) {return n + 100;}
     vector<int> frequencySort(vector<int>& nums) {
         map <int,int> frequency_map;
-        //int max_occurs = 0;
         for (auto n : nums) {
             frequency_map[n] ++;
-            //max_occurs = max(frequency_map[n],max_occurs);
         }
         set <int> frequency_map_T[200+1];
-        //for(int i = 1; i <= max_occurs; i++)    frequency_map_T[i] = new set<int>;
-        
         for(auto it : frequency_map){
             frequency_map_T[it.second].insert(it.first);
         }
