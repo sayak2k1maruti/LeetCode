067a485c599f87491f551a976a8a40d7d95f9c75
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-07-24 22:04:50 +0530

    Time: 132 ms (93.94%), Space: 77 MB (94.37%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -9,21 +9,22 @@ public:
     int decode(int n,vector <int> &mapping){
         if(! n) return mapping[0];
         int num = 0;
-        for (int i = 1;n;n/=10,i*=10){
+        for (int i = 1;n;n/=10,i*=10)
             num = i*mapping[n%10] + num;
-        }
+        
         return num;
     }
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        vector <pair<int,int>> num_map;
-        for (auto it = nums.begin(); it != nums.end(); it ++){
-            
-            int decoded_num = decode(*it,mapping);
-            num_map.push_back(make_pair(decoded_num,*it));
-        }
-        sort(num_map.begin(),num_map.end(),comparefn);
-        int indx = 0;
-        for (auto it : num_map) nums[indx++] = it.second;
+        pair <int,int> num_map[nums.size()];
+        
+        for (int i= 0 ; i < nums.size(); i++ )
+            num_map[i] = make_pair(decode(nums[i],mapping),nums[i]);
+
+        sort(num_map,num_map + nums.size(),comparefn);
+        
+        for (int i = 0 ; i < nums.size(); i++)  
+            nums[i] = num_map[i].second;
+        
         return nums;
     }
 };
