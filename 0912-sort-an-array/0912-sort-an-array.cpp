dbae0104cffc1a4c2fb9a14f0d8e3a6aeec90b6c
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-07-26 01:57:23 +0530

    Time: 64 ms (98.34%), Space: 69.8 MB (82.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -2,7 +2,13 @@ class Solution {
 public:
 
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int arr[50000*2+1] = {0};
+        for (auto n : nums)
+            arr[n + 50000] ++;
+        int indx = 0;
+        for(int i = 0; i < 50000*2+1; i++)
+            for (int j = 0 ; j < arr[i] ; j++)
+                nums[indx++] = i - 50000;
         return nums;
     }
 };
