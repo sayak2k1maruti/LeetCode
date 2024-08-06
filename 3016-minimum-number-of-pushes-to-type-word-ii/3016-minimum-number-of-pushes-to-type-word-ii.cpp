ba280555b4a7b9cd09457ecd439999fcdc127676
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-08-07 01:37:51 +0530

    Time: 59 ms (96.73%), Space: 25.4 MB (53.33%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -5,15 +5,23 @@ public:
         vector<int> charOccurances;
         for (auto c : word) charCount[c-'a']++;
         for(int i = 0 ; i < 26 ; i ++)  
-            if (charCount[i])
-                charOccurances.push_back(charCount[i]);
+            if (charCount[i])   charOccurances.push_back(charCount[i]);
+            
         sort(charOccurances.begin(),charOccurances.end(),greater<int>());
+        
         int keyCount = 0;
-        int numOfPress = 0;
+        int totalKeyPress = 0;
 
         for (auto count : charOccurances){
-            numOfPress += count * ((++keyCount-1)/8 + 1);
+            
+            totalKeyPress += count * ((++keyCount-1)/8 + 1);
+            /*
+                Keycount is b/w [1-8] => key needs to be pressed 1 time
+                Keycount is b/w [2-16] => key needs to be pressed 2 time
+                ...
+            */
+
         }
-        return numOfPress;
+        return totalKeyPress;
     }
 };
