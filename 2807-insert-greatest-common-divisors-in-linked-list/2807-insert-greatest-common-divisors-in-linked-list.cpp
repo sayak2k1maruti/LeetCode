9376ba54accf9cd1e342615d1cdea7735edfd81c
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-09-11 01:33:11 +0530

    Time: 150 ms (5.42%), Space: 39.9 MB (5.67%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -8,11 +8,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#define NUM_MAX 1000 
 class Solution {
 public:
+    int gcdMatrix[NUM_MAX+1][NUM_MAX+1];
+    Solution(){
+        for(int i = 0;i < NUM_MAX+1; i++){
+            for(int j = 0 ; j < NUM_MAX+1;j++){
+                gcdMatrix[i][j] = -1;
+            }
+        }
+    }
     int gcd(int a , int b){
+        if (gcdMatrix[a][b] != -1)  return gcdMatrix[a][b];
         int big = max(a,b),small = min(a,b);
-        if (big%small == 0)   return small;
+        if (big%small == 0){
+            gcdMatrix[a][b] = small;
+            gcdMatrix[b][a] = small;
+            return small;
+        }
         return gcd(big-small,small);
     }
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
