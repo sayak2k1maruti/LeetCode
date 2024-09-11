185614dcc02b5614ce0b6b22b0b025c96000377c
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-09-12 02:14:42 +0530

    Time: 31 ms (81.78%), Space: 16.6 MB (27.17%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,12 +1,11 @@
 BIT_LEN = 32
 class Solution:
-    @staticmethod
-    def count_set_bits(n):
+    def count_set_bits(self,n):
         count = 0
         while n:
             count += n%2
             n = n>>1
         return count
     def minBitFlips(self, start: int, goal: int) -> int:
-        return Solution.count_set_bits(start ^ goal)
+        return self.count_set_bits(start ^ goal)
         
