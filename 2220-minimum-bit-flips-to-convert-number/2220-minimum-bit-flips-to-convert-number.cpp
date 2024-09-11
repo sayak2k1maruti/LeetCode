55a459eddcd52c6c896ff9e6e7da74a97818c0dc
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-09-12 02:01:47 +0530

    Time: 3 ms (41.37%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -5,15 +5,10 @@ public:
         for(int i=0;n;n=n>>1,i++)
             arr[i] ^=  n%2;
     }
-    int count(int* arr){
-        int count = 0;
-        for(int i = 0 ; i < BIT_LEN; i++)   count += arr[i];
-        return count;
-    }
     int minBitFlips(int start, int goal) {
         int arr[BIT_LEN] = {0};
         updateBitArr(start,arr);
         updateBitArr(goal,arr);
-        return count(arr);
+        return count(arr,arr+BIT_LEN,1);
     }
 };
