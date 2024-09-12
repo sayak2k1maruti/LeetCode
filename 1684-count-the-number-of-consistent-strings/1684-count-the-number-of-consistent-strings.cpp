979520a7ca4aab9b3a6a66b8ce490617b038ae4a
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-09-12 23:20:48 +0530

    Time: 32 ms (96.74%), Space: 34 MB (27.48%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,17 @@
+#define N 26
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        bool allowedChar[N] = {false};
+        int count = 0;
+        for (auto c : allowed)  allowedChar[c-'a']=true;
+        
+        for (auto word : words){
+            bool flag = true;
+            for (auto c : word) 
+                if (! allowedChar[c-'a']) {flag = false;break;}
+            count += flag;
+        }
+        return count;
+    }
+};
