7e54942eb7476d710ebe0ac29e8b82c7294625b4
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-09-12 23:36:11 +0530

    Time: 18 ms (84.1%), Space: 18.5 MB (75.36%) - LeetHub

---
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -0,0 +1,24 @@
+#define N 26
+class Solution {
+public:
+    string getUniqueCharStr(string& s){
+        bool chars[N] = {false};
+        string ucs = "";
+        for(auto c : s) chars[c-'a'] = true;
+        for(int i =0 ; i < N ; i++)
+            if (chars[i])   ucs += (char)('a' + i);
+        return ucs;
+    }
+    int similarPairs(vector<string>& words) {
+        int pairsCount = 0;
+        map<string,int> wordsMap;
+        for(auto word : words){
+            wordsMap[getUniqueCharStr(word)]++;
+        }
+        for(auto it : wordsMap){
+            if (it.second)
+                pairsCount += it.second * (it.second - 1)/2;
+        }
+        return pairsCount;
+    }
+};
