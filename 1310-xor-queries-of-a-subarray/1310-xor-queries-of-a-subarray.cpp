6ba3b54ee868e52d46bd22497aab5ef7c75948d4
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2024-09-14 02:43:21 +0530

    Time: 1147 ms (14.85%), Space: 41.2 MB (98.18%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int xor_arr(int start,int end,vector <int>& arr){
-        int xor_value = arr[start];
-        for (int i = start+1;i <= end; i++){
-            xor_value ^= arr[i];
-        }
-        return xor_value;
+        if(start == end)    return arr[start];
+        return arr[start] ^ xor_arr(start+1,end,arr);
     }
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> result_arr(queries.size(),0);
