d5b7f2f772da83d25dec05f54417bcbd7da61fd8
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-01-27 23:30:53 +0530

    Time: 43 ms (64.91%), Space: 73.6 MB (38.25%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool isReachable(vector<set<int>> &preq,int a , int b,vector<vector<int>> &cache){
+    bool isReachable(vector<vector<int>> &preq,int a , int b,vector<vector<int>> &cache){
         if(cache[a][b] != -1)   return cache[a][b];
-        if( preq[a].find(b) != preq[a].end())   return cache[a][b] = true;
+        //if( preq[a].find(b) != preq[a].end())   return cache[a][b] = true;
         for(auto c : preq[a])
-            if (isReachable(preq,c,b,cache))
+            if (c==b || isReachable(preq,c,b,cache))
                 return cache[a][b] = true;
         return cache[a][b] = false;
     }
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<set<int>> preq(numCourses,set<int>());
+        vector<vector<int>> preq(numCourses,vector<int>());
         vector <bool> ans;
         vector<vector<int>> cache(numCourses+1,vector<int>(numCourses+1,-1));
         for(auto coursePair : prerequisites){
-            preq[coursePair[0]].insert(coursePair[1]);
+            preq[coursePair[0]].push_back(coursePair[1]);
         }
         for (auto query : queries){
             ans.push_back(isReachable(preq,query[0],query[1],cache));
