5c7e6fbab7d256cd9994b493accf85658f16b58b
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-01-29 01:24:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int catchMaxFish(vector<vector<int>> &grid,int r,int c){
+        if(r<0 || c <0 || r >= grid.size() || c >=grid[0].size())  return 0;
+        int totalFish = grid[r][c];
+        grid[r][c] = -1;//i.e, this cell is already visited in this search
+        if(totalFish <= 0) return 0;
+
+        int ans= totalFish + 
+                catchMaxFish(grid,r,c+1) +
+                catchMaxFish(grid,r,c-1) +
+                catchMaxFish(grid,r+1,c) +
+                catchMaxFish(grid,r-1,c) ;
+        grid[r][c] = totalFish; // reverting back to original value after run
+        return ans;
+    }
+    int findMaxFish(vector<vector<int>>& grid) {
+        int fish = 0;
+        for(int r = 0 ; r < grid.size(); r++)
+            for(int c = 0 ; c < grid[0].size();c++)
+                fish = max(fish,catchMaxFish(grid,r,c));
+        return fish;
+    }
+};
