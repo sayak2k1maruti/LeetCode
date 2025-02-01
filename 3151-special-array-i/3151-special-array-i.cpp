4a8a4dde5a21e8843e51826ebd9305f21f96aff8
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-02-02 04:57:21 +0530

    Time: 0 ms (100%), Space: 27.7 MB (61.84%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         int n = nums.size();
-        bool isOdd = nums[0] & 1;
         for(int i = 1;i < n; i++){
-            if ((nums[i] & 1) == isOdd) return false;
-            isOdd = nums[i] & 1;
+            if(!((nums[i] ^ nums[i-1]) & 1)) return false;
         }
         return true;
     }
