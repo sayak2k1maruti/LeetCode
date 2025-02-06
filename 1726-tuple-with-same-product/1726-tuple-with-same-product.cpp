f72304236aad6f77794289ae3a322b42ab751590
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-02-07 01:50:51 +0530

    Time: 787 ms (10.53%), Space: 99.4 MB (7.12%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    int binarySearch(int n,int l,int r,vector<int> & nums){
-        int mid;
-        while(l<=r){
-            mid = l + (r-l)/2;
-            if(nums[mid]==n)    return mid;
-            if(nums[mid] > n)   r = mid-1;
-            else l = mid+1;
-        }
-        return -1;
-    }
     int tupleSameProduct(vector<int>& nums) {
-        int count = 0;
+        int n = nums.size(),count = 0;
         map<int,int> productMap;
-        for(int i = 0 ; i < nums.size(); i++){
-            for(int j= i+1; j < nums.size() ; j++)
+        for(int i = 0 ; i < n; i++){
+            for(int j= i+1; j < n ; j++)
                 productMap[nums[i]*nums[j]] ++;
         }
         for(auto& [product ,n] : productMap){
