04df57b669376be7f23f528ee55f4ae1532f0163
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-02-10 23:37:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (87.14%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -3,21 +3,29 @@ public:
     bool isNonDigit(char c){
         return ! (c >= '0' && c<='9');
     }
+    void markNearestNonDigitLeft(string &s,int index){
+        for(int i = index-1; i>=0; i--){
+            if(isNonDigit(s[i])){
+                s[i] = '0';
+                return;
+            }
+        }
+    }
+    string deleteAllNonDigits(string& s){
+        string ans = "";
+        for(auto &c: s){
+            if(isNonDigit(c))   ans += c;
+        }
+        return ans;
+    }
     string clearDigits(string s) {
         int n = s.length();
         for(int i = 0;i < n ; i++){
             if(!isNonDigit(s[i])){
-                for(int j = i-1; j >= 0 ; j--){
-                    if(isNonDigit(s[j])){
-                        s[j] = '0';break;
-                    }
-                }
+                markNearestNonDigitLeft(s,i);
             }
         }
-        string ans = "";
-        for(auto c: s){
-            if(isNonDigit(c))   ans += c;
-        }
-        return ans;
+
+        return deleteAllNonDigits(s);
     }
 };
