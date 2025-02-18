0f704b65ff6a68fb2ab53536595ed4fcb6fe19b1
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-02-19 01:00:09 +0530

    Time: 3 ms (26.77%), Space: 9.3 MB (7.71%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string backtrack(string pattren,int index,string num,bool *usedDigit){
+        if(index > pattren.length())    return num;
+        cout << num << endl;
+        for(int digit = 1;digit <= 9; digit ++){
+            if(usedDigit[digit])    continue;
+            if ((index!=0) && (pattren[index-1] == 'I') && (num[index-1]-'0' > digit)) continue;
+            if ((index!=0) && (pattren[index-1] == 'D') && (num[index-1]-'0' < digit)) continue;
+            usedDigit[digit] = true;
+            string ans = backtrack(pattren,index+1,num+to_string(digit),usedDigit);
+            if(ans.length()) return ans;
+            usedDigit[digit] = false;
+        }
+        return "";
+    }
+    string smallestNumber(string pattern) {
+        string number = "";
+        bool usedDigit[10] = {false};
+        return backtrack(pattern,0,number,usedDigit);
+    }
+};
