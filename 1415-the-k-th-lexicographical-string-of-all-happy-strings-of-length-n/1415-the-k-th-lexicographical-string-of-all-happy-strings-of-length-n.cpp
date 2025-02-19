7229ff691feb5d6b5d18eedc0d82ce37a2ec66b1
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-02-20 01:50:22 +0530

    Time: 4 ms (64.1%), Space: 8.9 MB (79.35%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,22 @@
+char arr[] = {'a','b','c'};
+class Solution {
+public:
+    
+    string generate(string s,int n,int &count,int &k){
+        if(s.length()==n)    {count++;}
+        if(s.length()>n)    return "";
+        if(count == k)  return s;
+        for(int i = 0 ;i < 3 ;i++){
+            if((s.length()==0) || (arr[i] != s[s.length()-1]))
+            {string x = generate(s+arr[i],n,count,k);
+            if(x.length()) return x;}
+        }
+        return "";
+    }
+    string getHappyString(int n, int k) {
+        int count = 0;
+        string ans = "";
+        ans = generate(ans,n,count,k);
+        return ans;
+    }
+};
