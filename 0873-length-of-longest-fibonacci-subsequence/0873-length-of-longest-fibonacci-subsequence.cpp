0066585e1ac143da70875204b49ff978cecb65e6
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-02-28 02:27:27 +0530

    Time: 281 ms (67.7%), Space: 15.7 MB (81.16%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -5,18 +5,18 @@ public:
         for(int i = 0 ; i < arr.size(); i++)    mp[arr[i]] = i;
         return mp;
     }
-    int flength(unordered_map <int,int> & mp,int a, int b){
-        if(mp.count(a+b)){
-            return 1 + flength(mp,b,a+b);
+    int flength(unordered_set <int> & st,int a, int b){
+        if(st.count(a+b)){
+            return 1 + flength(st,b,a+b);
         }
         return 0;
     }
     int lenLongestFibSubseq(vector<int>& arr) { 
         int maxLen = 0;
-        unordered_map<int,int> im = indxMap(arr);
+        unordered_set<int> st(arr.begin(),arr.end());
         for(int l = 0 ; l <arr.size()-1; l++){
             for(int r = l+1; r < arr.size(); r++){
-                maxLen = max(maxLen,flength(im,arr[l],arr[r]));
+                maxLen = max(maxLen,flength(st,arr[l],arr[r]));
             }
         }
         return maxLen ? maxLen + 2 : 0;
