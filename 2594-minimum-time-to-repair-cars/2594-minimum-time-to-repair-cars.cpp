3a9013837492708d0c7a36ab8785d161b009cd97
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-03-16 20:19:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canBeRepaired(vector<int> & ranks,long long time,int cars){
+        // r*n^2 < time
+        // n^2 < time/r
+        // n < sqrt(time/r)
+        int rqCars = 0;
+        for(auto &r : ranks){
+            rqCars += sqrt(time/r);
+        }
+        return rqCars>=cars;
+    }
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long time = (long long)ranks[0] * (long long)cars * (long long)cars;
+        long long startT = 1,endT = time, ans = 0;
+        while(startT <= endT){
+            cout << startT << " " << endT << endl;
+            long long mid = startT + (endT - startT)/2;
+            if(canBeRepaired(ranks,mid,cars)){
+                endT = mid-1;
+                ans = mid;
+            }else{
+                startT = mid+1;
+            }
+        }
+        return ans;
+    }
+};
