e0a063f88f73d2bca4ab950f0c431cc971a313c9
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-03-19 19:41:47 +0530

    Time: 7 ms (59.94%), Space: 140.1 MB (47.55%) - LeetHub

---
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void invrt(int &i){
+        i = (!i);return;
+    }
+    int minOperations(vector<int>& nums) {
+        int count = 0;
+        for (int i = 0 ; i < nums.size() ; i++ ){
+            if (nums[i]) continue;
+            if(i+2 >= nums.size())  return -1;
+            for(int z = 0 ; z < 3; z++) invrt(nums[i+z]);
+            count++;
+        }
+        return count;
+    }
+};
+
+/*
+0 1 1 1 0 0
+1 0 0 1 0 0
+1 1 1 0 0 0
+1 1 1 1 1 1
+
+0 1 1 1
+1 0 0 1
+1 1 1 0
+*/
