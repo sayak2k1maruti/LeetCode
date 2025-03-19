60ab75f82e83c09557c0b9fc0c535c9930cad4b9
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-03-19 19:45:51 +0530

    Time: 11 ms (31.84%), Space: 140.1 MB (15.99%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -8,7 +8,7 @@ public:
         for (int i = 0 ; i < n ; i++ ){
             if (nums[i]) continue;
             if(i+2 >= n)  return -1;
-            for(int z = 1 ; z < 3; z++) invrt(nums[i+z]);
+            for(int z = 1 ; z < 3; z++) nums[i+z] ^= 1;
             count++;
         }
         return count;
