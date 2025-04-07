02d4ab64ade80b7c2ea22244420f734ab434624c
Sayak <64575577+sayak2k1maruti@users.noreply.github.com>
2025-04-08 01:15:19 +0530

    Time: 67 ms (85.32%), Space: 99.9 MB (49.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool findSum(vector<int>& nums,int i,int sum,int** cache){
+    bool findSum(vector<int>& nums,int n,int i,int sum,int** cache){
         
         if(sum < 0) return false;
-        if(i >= nums.size())    return false;
+        if(i >= n)    return false;
 
         if(cache[i][sum] != -1)   return cache[i][sum];
         if(sum==0)  return cache[i][0] = true;
 
-        return cache[i][sum] = findSum(nums,i+1,sum-nums[i],cache) || findSum(nums,i+1,sum,cache); 
+        return cache[i][sum] = findSum(nums,n,i+1,sum-nums[i],cache) || findSum(nums,n,i+1,sum,cache); 
     }
     bool canPartition(vector<int>& nums) {
-       int sum = 0;
+       int sum = 0,  n = nums.size();
        for(auto &i : nums)  sum += i;
        if(sum & 1) return false;
+
        int **cache;
-       cache = new int *[nums.size()+1];
-       for(int i = 0 ; i <= nums.size(); i++){
+       cache = new int *[n+1];
+       for(int i = 0 ; i <= n; i++){
            cache[i] = new int[sum/2 + 1];
            for(int j = 0 ; j <= sum/2 ; j ++){
                 cache[i][j] = -1;
            }
        }
-       for(int i = 0 ; i <= nums.size(); i++)
+       for(int i = 0 ; i <= n; i++)
             for(int j = 0 ; j <= sum/2 ; j++)   
                 cache[i][j] = -1;
 
-       return findSum(nums,0,sum/2,cache);
+       return findSum(nums,n,0,sum/2,cache);
     }
 };
